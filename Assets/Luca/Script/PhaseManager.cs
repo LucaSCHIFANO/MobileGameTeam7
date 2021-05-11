124b8af14389dca536bd7adf2245e0330e8e6f3a
LucaSchifano <luca.schifano005@gmail.com>
2021-05-11 14:53:01 +0200

    Mouvement Suite 2 ( enemy )

---
+++ Assets/Luca/Script/PhaseManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhaseManager : MonoBehaviour
+{
+    public actualPhase phase;
+    public bool oneTime = true;
+
+    public bool selectFoe = false;
+
+    public GameObject uiColor;
+
+    public enum actualPhase
+    {
+        PLAYER,
+        ENEMY,
+    }
+
+
+    private static PhaseManager _instance = null;
+
+    public static PhaseManager Instance
+    {
+        get => _instance;
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
+    void Start()
+    {
+        phase = actualPhase.PLAYER;
+        Debug.Log("Player Phase");
+    }
+
+    void Update()
+    {
+        if (phase == actualPhase.ENEMY && !oneTime)
+        {
+            oneTime = true;
+            CharacterManager.Instance.enemiesMovement();
+            uiColor.GetComponent<Image>().color = Color.red;
+
+        }
+        else if (phase == actualPhase.PLAYER && !oneTime)
+        {
+            oneTime = true;
+            CharacterManager.Instance.resetAllCharacter();
+            uiColor.GetComponent<Image>().color = Color.blue;
+
+        }
+    }
+
+
+    public void checkAllPlayer()
+    {
+        int number = 0;
+        foreach (var player in CharacterManager.Instance.playerList)
+        {
+            if (player.GetComponent<PlayerMovement>().state != PlayerMovement.States.WAIT)
+            {
+                number++;
+            }
+        }
+        if (number == 0)
+        {
+            phase = actualPhase.ENEMY;
+            oneTime = false;
+            Debug.Log("Enemy Phase");
+        }
+    }
+
+    public void checkAllEnemies()
+    {
+        int number = 0;
+        foreach (var enemy in CharacterManager.Instance.enemyList)
+        {
+            if (enemy.GetComponent<Enemy>().characterState != PlayerMovement.States.WAIT)
+            {
+                number++;
+            }
+        }
+
+        if (number == 0)
+        {
+            phase = actualPhase.PLAYER;
+            oneTime = false;
+            Debug.Log("Player Phase");
+            CharacterManager.Instance.countMoveEnemy = 0;
+        }
+        else
+        {
+            CharacterManager.Instance.countMoveEnemy++;
+            CharacterManager.Instance.enemiesMovement();
+        }
+    }
+}
